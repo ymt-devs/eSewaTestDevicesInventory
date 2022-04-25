5f469063730abca9a8b9cd74bf303c41cca814f9
Yojan Man Tuladhar <yojantuladhar@gmail.com>
2022-04-25 09:24:19 +0545

    refresher

--- app/src/main/java/com/esewa/tdi/data/Cable.kt
+++
@@ -1,7 +0,0 @@
-package com.esewa.tdi.data
-
-data class Cable(var Name : String ?= null, var id : String?= null, var Department : String ?= null,
-                 var Device : String?= null
-){
-    override fun toString(): String = Name?:""
-}
--- app/src/main/java/com/esewa/tdi/interfaces/CardClickListener.kt
+++
@@ -1,8 +0,0 @@
-package com.esewa.tdi.interfaces
-
-import com.esewa.tdi.data.User
-
-interface CardClickListener {
-
-    fun onItemClick(user: User)
-}
